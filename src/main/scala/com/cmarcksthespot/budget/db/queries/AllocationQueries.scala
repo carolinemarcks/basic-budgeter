0023e0549325703f07940b71ac8bbe8518e1c3c0
Caroline Marcks <caroline.marcks@gmail.com>
2017-07-28 16:16:59 -0400

    implement some basic allocation queries

--- src/main/scala/com/cmarcksthespot/budget/db/queries/AllocationQueries.scala
+++ src/main/scala/com/cmarcksthespot/budget/db/queries/AllocationQueries.scala
@@ -1,6 +1,6 @@
 package com.cmarcksthespot.budget.db.queries
 
-import com.cmarcksthespot.budget.db.model.Allocations
+import com.cmarcksthespot.budget.db.model.{Allocation, AllocationType, Allocations}
 import slick.driver.MySQLDriver.api._
 import slick.jdbc.meta.MTable
 import slick.lifted.TableQuery
@@ -8,8 +8,15 @@ import slick.lifted.TableQuery
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
+
 trait AllocationQueries {
   def createTable(): Future[Unit]
+
+  def getAllocations(): Future[Seq[Allocation]]
+
+  def createAllocation(name: String, saved: Int, weight: Int, cap: Int, allocationType: AllocationType): Future[Allocation]
+
+  def updateAllocation(id: Int, name: String, saved: Int, weight: Int, cap: Int, allocationType: AllocationType): Future[Option[Allocation]]
 }
 object AllocationQueries {
   def apply(db: Database) = new AllocationQueriesImpl(db)
@@ -28,5 +35,41 @@ private[db] class AllocationQueriesImpl(db: Database) extends AllocationQueries 
         Future.successful(())
       }
     }
+  }
+
+  override def getAllocations(): Future[Seq[Allocation]] = {
+    db.run(allocations.result)
+  }
+
+  private val allocationInsertQuery = allocations returning (allocations.map(_.id)) into ((row, id) => row.copy(id = id))
+
+  override def createAllocation(name: String, saved: Int, weight: Int, cap: Int, allocationType: AllocationType): Future[Allocation] = {
+    db.run(allocationInsertQuery += Allocation(0, name, saved, weight, cap, allocationType))
+  }
+
+  override def updateAllocation(id: Int, name: String, saved: Int, weight: Int, cap: Int, allocationType: AllocationType): Future[Option[Allocation]] = {
+    updateAllocation(id, { a => a.copy(
+      name = name,
+      saved = saved,
+      weight = weight,
+      cap = cap,
+      allocationType = allocationType
+    )
+    })
+  }
+
+  private def updateAllocation(allocation: Allocation): DBIO[Option[Allocation]] = {
+    allocations.filter(_.id === allocation.id.bind).update(allocation).map {
+      case 0 => None
+      case _ => Some(allocation)
+    }
+  }
+
+  private def updateAllocation(id: Int, op: Allocation => Allocation): Future[Option[Allocation]] = {
+    val query = allocations.filter(_.id === id.bind).result.headOption.flatMap {
+      case None => DBIO.successful(None)
+      case Some(allocation) => updateAllocation(op(allocation))
+    }
+    db.run(query)
   }
 }
