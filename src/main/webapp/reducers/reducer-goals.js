1320e5ac5a5f8e859f791cebbbaa488d050d0083
Caroline Marcks <caroline.marcks@gmail.com>
2017-07-30 16:36:39 -0400

    bubble up highest weighted goals

--- src/main/webapp/reducers/reducer-goals.js
+++ src/main/webapp/reducers/reducer-goals.js
@@ -4,9 +4,9 @@ import _ from 'lodash';
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_GOALS:
-      return _.sortBy(action.payload.data, "name");
+      return _.orderBy(action.payload.data, ["weight", "name"], ["desc", "asc"]);
     case CREATE_GOAL:
-      return _.sortBy([ ...state, action.payload.data ], "name");
+      return _.orderBy([ ...state, action.payload.data ], ["weight", "name"], ["desc", "asc"]);
     default:
       return state;
     }
